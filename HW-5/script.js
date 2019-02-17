009a0ffff2017540ffbda61787db3e5c6dd8e90b
Leah Becker <leah.becker@umconnect.umt.edu>
2019-02-16 18:48:12 -0700

    update hw-5

--- HW-5/script.js
+++ HW-5/script.js
@@ -1,11 +1,10 @@
 
 // Array for the back of the cards
-var cards = ["image1","image2","image3","image4", "image 5",
-            "image6", "image7", "image8", "image9", "image10"];
+var cards = ["image1","image2","image3","image4", "image 5","image6", "image7", "image8", "image9", "image10"];
 
-var blankCard = "images/blankCard.jpg";
+var blankCard = "images/aceOfSpades.png";
 
-var cardReveal = new Array();
+var actualCard = new Array();
 
 
 
@@ -18,9 +17,22 @@ function printCards(){
   }
 }
 
-function randomizeCards (cards){
-  var actualCardPath
-  for(var i=0; i< cards.length; i++){
+function randomizeCards (){
+  var actualCardPath = ["aceOfSpades.PNG","eightOfDiamonds.PNG","kingOfHearts.JPG","tenOfClubs.PNG","twoOfSpades.PNG"];
 
+  var count = [0,0,0,0,0];
+
+  while(actualCard.length < 10){
+
+    var randomNum = math.floor(math.random()* actualCardPath.length);
+
+    if(count[randomNum] < 2){
+      actualCard.push(actualImagePath[randomNum]);
+      count[randomNum] ++;
+    }
   }
+}
+
+function flipImage(cardPlace){
+  document.getElementById(actualCard[cardPlace]).src = actualCard[cardPlace];
 }
