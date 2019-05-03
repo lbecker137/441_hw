cb0d97c4262aa0ee50b391157b0ee401b3212101
Leah Becker <leah.becker@umconnect.umt.edu>
2019-05-03 16:27:21 -0600

    more final stuff

--- Final/scripts/canvas.js
+++ Final/scripts/canvas.js
@@ -4,11 +4,10 @@ background.src = "images/background.jpg";
 var fruit = document.createElement("img");
 fruit.src= "images/fruit.png";
 
-var snake = [];
-snake[0]= "yes";
+var snake = new Array;
 
-var foodx = Math.floor(Math.random()*30+1)*20;
-var foody = Math.floor(Math.random()*30+1)*20;
+var foodX = Math.floor(Math.random()*30+1)*20;
+var foodY = Math.floor(Math.random()*30+1)*20;
 
 var score = 0;
 var checkEnd = 1;
@@ -18,6 +17,54 @@ var yPosition = 20;
 
 var direction = "right";
 
+// **** begin class
+
+class Square{
+
+    constructor(xCoord,yCoord,color)
+    {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        //this.objectHeight = objectHeight;
+        //this.objectWidth = objectWidth;
+        this.color = color;
+    }
+
+    get x()
+    {
+        return this.xCoord;
+    }
+
+    set x(value)
+    {
+        this.xCoord = value;
+    }
+    get y()
+    {
+        return this.yCoord;
+    }
+    set y(value)
+    {
+        this.yCoord = value;
+    }
+
+    /*get height()
+    {
+        return this.objectHeight;
+    }
+
+    get width()
+    {
+        return this.objectWidth;
+    }
+    */
+    get mainColor()
+    {
+        return this.color;
+    }
+}
+
+// **** end class
 
 $(document).ready(function(){
 
@@ -35,6 +82,8 @@ function setup(){
   canvas = document.getElementById("myCanvas");
   ctx = canvas.getContext("2d");
 
+  snake.push(new Square(20,20,"green"));
+
   if(checkEnd==1){
     setInterval(draw,100);
   }
@@ -45,29 +94,44 @@ function setup(){
 
 }
 
+// fruit
+function checkFruit(){
+  if(snake[0].x+21>= foodX+1 && snake[0].y+21 >= foodY+1 && snake[0].x <= foodX+19 && snake[0].y <= foodY+19){
+    foodX = Math.floor(Math.random()*30+1)*20;
+    foodY = Math.floor(Math.random()*30+1)*20;
+    score++;
+    if(direction == "up"){
+      snake.push(new Square(snake[0].x, snake[0].y + snake.length*20, "black"));
+    }
+
+  }
+}
+
 function draw(){
   ctx.drawImage(background,0,0);
   for(var i = 0; i <= snake.length; i++){
     ctx.fillStyle = "green";
-    ctx.fillRect( xPosition, yPosition, 20, 20);
+    ctx.fillRect( snake[i].x, snake[i].y, 20, 20);
   }
 
-  ctx.drawImage(fruit,foodx,foody,20,20);
+  ctx.drawImage(fruit,foodX,foodY,20,20);
 
   if(direction == "right"){
-    xPosition +=20;
-    console.log(xPosition);
+    for (var i = 1; i<= snake.length; i++){
+      snake[i].x -= 20;
+    }
+    //console.log(xPosition);
   }
   else if(direction == "left"){
-    xPosition -= 20;
+    snake[0].x -= 20;
   }
   else if(direction == "up"){
-    yPosition -= 20;
+    snake[0].y -= 20;
   }
   else{
-    yPosition += 20;
+    snake[0].y += 20;
   }
-  //checkEnd();
+  checkFruit();
 
 }
 
@@ -77,11 +141,9 @@ function getKey(event){
 
     if(actualLetter == "w"){
         direction = "up";
-        console.log(direction);
     }
     if(actualLetter == "s"){
         direction = "down";
-        console.log(direction);
     }
     if(actualLetter == "a"){
         direction = "left";
