90ee91e203412ee8094951b6ab53a63c288f9fca
Leah Becker <leah.becker@umconnect.umt.edu>
2019-05-03 20:08:12 -0600

    final

---
+++ Final/readme.md
@@ -0,0 +1,3 @@
+## Final
+
+For this project I created a game where the player roves the canvas. If they hit an object, their score increases and their player gains a square. If they run off the page, the game ends.
--- Final/scripts/canvas.js
+++ Final/scripts/canvas.js
@@ -3,14 +3,16 @@ var background = document.createElement("img");
 background.src = "images/background.jpg";
 var fruit = document.createElement("img");
 fruit.src= "images/fruit.png";
+var endBackground = document.createElement("img");
+endBackground.src="images/endbg.jpg"
 
-var snake = new Array;
+var blob = new Array;
 
 var foodX = Math.floor(Math.random()*30+1)*20;
 var foodY = Math.floor(Math.random()*30+1)*20;
 
 var score = 0;
-var checkEnd = 1;
+var game = setInterval(draw,100);
 
 var xPosition = 20;
 var yPosition = 20;
@@ -21,12 +23,12 @@ var direction = "right";
 
 class Square{
 
-    constructor(xCoord,yCoord,color)
+    constructor(xCoord,yCoord,objectHeight,objectWidth, color)
     {
         this.xCoord = xCoord;
         this.yCoord = yCoord;
-        //this.objectHeight = objectHeight;
-        //this.objectWidth = objectWidth;
+        this.objectHeight = objectHeight;
+        this.objectWidth = objectWidth;
         this.color = color;
     }
 
@@ -48,7 +50,7 @@ class Square{
         this.yCoord = value;
     }
 
-    /*get height()
+    get height()
     {
         return this.objectHeight;
     }
@@ -57,7 +59,6 @@ class Square{
     {
         return this.objectWidth;
     }
-    */
     get mainColor()
     {
         return this.color;
@@ -82,56 +83,90 @@ function setup(){
   canvas = document.getElementById("myCanvas");
   ctx = canvas.getContext("2d");
 
-  snake.push(new Square(20,20,"green"));
+  blob.push(new Square(20,20,20,20,"green"));
 
-  if(checkEnd==1){
-    setInterval(draw,100);
-  }
-
-  if(checkEnd==0){
-    endGame();
-  }
 
 }
 
 // fruit
 function checkFruit(){
-  if(snake[0].x+21>= foodX+1 && snake[0].y+21 >= foodY+1 && snake[0].x <= foodX+19 && snake[0].y <= foodY+19){
-    foodX = Math.floor(Math.random()*30+1)*20;
-    foodY = Math.floor(Math.random()*30+1)*20;
-    score++;
-    if(direction == "up"){
-      snake.push(new Square(snake[0].x, snake[0].y + snake.length*20, "black"));
+  var num = Math.floor(Math.random()*20);
+  for(var i = 0; i< blob.length; i++){
+    if(blob[i].x+21>= foodX+1 && blob[i].y+21 >= foodY+1 && blob[i].x <= foodX+19 && blob[i].y <= foodY+19){
+      foodX = Math.floor(Math.random()*30+1)*20;
+      foodY = Math.floor(Math.random()*30+1)*20;
+      score++;
+      if(direction=="up"){
+        blob.push(new Square(blob[0].xCoord, blob[0].yCoord + num*score, 20, 20, "grey"));
+      }
+      if(direction=="down"){
+        blob.push(new Square(blob[0].xCoord, blob[0].yCoord - num*score, 20, 20, "purple"));
+      }
+      if(direction=="left"){
+        blob.push(new Square(blob[0].xCoord- num*score, blob[0].yCoord, 20, 20, "pink"));
+      }
+      if(direction=="right"){
+        blob.push(new Square(blob[0].xCoord+num*score, blob[0].yCoord, 20, 20, "blue"));
+      }
+      console.log(blob.length)
     }
-
   }
+}
+
+function checkEdges(){
+  for(var i = 0; i<blob.length; i++){
+    if(blob[i].xCoord <0 || blob[i].xCoord +20 > 800 || blob[i].yCoord<0 || blob[i].yCoord>600){
+      endGame();
+    }
+  }
+}
+
+function endGame(){
+  clearInterval(game);
+
+  ctx.drawImage(endBackground,0,0);
+
+  ctx.font = "50px Arial";
+  ctx.fillStyle = "white";
+  ctx.fillText("Game Over", 250, 150);
+  ctx.font= "20px Arial";
+  ctx.fillText("score: " + score, 320, 200);
 }
 
 function draw(){
   ctx.drawImage(background,0,0);
-  for(var i = 0; i <= snake.length; i++){
-    ctx.fillStyle = "green";
-    ctx.fillRect( snake[i].x, snake[i].y, 20, 20);
+  for(var i = 0; i <= blob.length; i++){
+    for(var i = 0; i< blob.length; i++){
+      ctx.fillStyle = blob[i].color;
+      ctx.fillRect( blob[i].xCoord, blob[i].yCoord, 20, 20);
+    }
   }
 
   ctx.drawImage(fruit,foodX,foodY,20,20);
 
   if(direction == "right"){
-    for (var i = 1; i<= snake.length; i++){
-      snake[i].x -= 20;
+    for(var i = 0; i<= blob.length-1; i++){
+      blob[i].xCoord += 20;
     }
-    //console.log(xPosition);
   }
   else if(direction == "left"){
-    snake[0].x -= 20;
+    for(var i = 0; i<=blob.length-1; i++){
+      blob[i].xCoord -= 20;
+    }
   }
+
   else if(direction == "up"){
-    snake[0].y -= 20;
+    for(var i= 0; i<= blob.length-1; i++){
+        blob[i].yCoord -= 20;
+    }
   }
   else{
-    snake[0].y += 20;
+    for( var i=0; i<= blob.length-1; i++){
+      blob[i].yCoord += 20;
+    }
   }
   checkFruit();
+  checkEdges();
 
 }
 
